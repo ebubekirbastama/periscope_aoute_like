0cd460f117d88474ae6ca37d0276de60c2c50c70
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2019-12-15 23:46:48 +0300

    Add files via upload

---
+++ periscop.js
@@ -0,0 +1,5 @@
+function myTimer() {
+  document.getElementsByClassName('Touchable is-custom UserHeart UserHeart--participantOrange')[0].click();
+ }
+var myVar = setInterval(myTimer, 1000);
+console.log(myVar);
